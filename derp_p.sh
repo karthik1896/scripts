aabdc7e9a45f843c2bfa5155fb353ba43eaff0f7
karthik1896 <karthik1896@outlook.com>
2020-12-27 17:08:04 +0530

    Lets Add Derp pie script

---
+++ derp_p.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/karthik1896/device_asus_X00T.git -b derp-p device/asus/X00T
+git clone https://github.com/DerpGang/proprietary_vendor_asus.git -b pie vendor/asus 
+git clone https://github.com/DerpGang/kernel_asus_sdm660.git -b 9.0 kernel/asus/sdm660
+git clone https://github.com/karthik1896/hardware_qcom_audio_sdm660.git hardware/qcom/audio/sdm660
+git clone https://github.com/karthik1896/hardware_qcom_media_sdm660.git hardware/qcom/media/sdm660
+git clone https://github.com/karthik1896/hardware_qcom_display_sdm660.git hardware/qcom/display/sdm660
+git clone https://github.com/karthik1896/hardware_qcom_power.git hardware/qcom/power
+
